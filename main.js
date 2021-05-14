e8d1098ca9d581d006ab842c6644225d4fb4eb83
user name <abdalafo.abubaker.com>
2021-05-14 16:24:50 +0300

    finish  getArticlesByAuthor

--- main.js
+++ main.js
@@ -24,32 +24,58 @@ const articles = [
 ];
 app.use(express.json());
 
-const getAllArticles = () => {
-  app.get("/articles", (req, res) => {
+const getAllArticles = (req, res) => {
+  res.status(200);
+  res.json(articles);
+};
+app.get("/articles", getAllArticles);
+
+const getAnArticleById = (req, res) => {
+  const id = req.query.id;
+  const found = articles.find((element, i) => {
+    console.log(element.id, element.id === id);
+    return element.id === Number(id);
+  });
+  if (found) {
     res.status(200);
-    res.json(articles);
-  });
+    res.json(found);
+  } else {
+    res.status(404);
+    res.json("not found");
+  }
 };
 
-const getAnArticleById = () => {
-  app.get(`/articles/:id`, (req, res) => {
-    const id = req.params.id;
-    const found = articles.find((element, i) => {
-      console.log(element.id, element.id === id);
-      return element.id == id;
-    });
-    if (found) {
-      res.status(200);
-      res.json(found);
-    } else {
-      res.status(404);
-      res.json("not found");
-    }
+app.get(`/articles/:id`, getAnArticleById);
+
+const getArticlesByAuthor = (req, res) => {
+  const author = req.query.author;
+  const found = articles.find((element, i) => {
+    console.log(element.author, author);
+    return element.author === author;
   });
+  if (found) {
+    res.status(200);
+    res.json(found);
+  } else {
+    res.status(404);
+    res.json("not found");
+  }
 };
 
-// getAllArticles();
-getAnArticleById();
+app.get(`/articles/search_1`, getArticlesByAuthor);
+
+const createNewArticle = (req, res) => {
+  const newArticle = {
+    title: req.body.title,
+    description: req.body.description,
+    author: req.body.author,
+  };
+  articles.push(newArticle);
+  res.status(201);
+  res.json(newArticle);
+};
+app.post("/articles", createNewArticle);
+
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
