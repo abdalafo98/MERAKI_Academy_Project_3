772fca117d2f9c34f2fe2f1367debd5716f6c20a
user name <abdalafo.abubaker.com>
2021-05-14 10:15:46 +0300

    finish getAllArticles

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+
+const articles = [
+  {
+    id: 1,
+    title: "How I learn coding?",
+    description: "Lorem, Quam, mollitia.",
+    author: "Jouza",
+  },
+  {
+    id: 2,
+    title: "Coding Best Practices",
+    description: "Lorem, ipsum dolor sit, Quam, mollitia.",
+    author: "Besslan",
+  },
+  {
+    id: 3,
+    title: "Debugging",
+    description: "Lorem, Quam, mollitia.",
+    author: "Jouza",
+  },
+];
+app.use(express.json());
+
+const getAllArticles = () => {
+  app.get("/articles", (req, res) => {
+    res.status(200);
+    res.json(articles);
+  });
+};
+
+getAllArticles();
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
